bca9efe7829faf6905c8d43a90ae5dd4d71abbd2
jay <nielcansino@gmail.com>
2019-06-30 17:20:21 +0900

    working app_median_10k

--- src/app_median_10k.cpp
+++ src/app_median_10k.cpp
@@ -13,6 +13,7 @@ int main(){
   //-----------------------------------
   //Variables for the median
   int d;  //data integer
+  long long int sum = 0;  //sum accumulator
   int i = 0;  //counter
   std::vector <int> median; //median vector
   MinHeap *h_high = new MinHeap;  //heap that contains the upper half parsed integers
@@ -33,14 +34,35 @@ int main(){
     i++;
     //convert string line to integer
     d = std::stoi(s);
-    std::cout << d << std::endl;
 
     //as of this point i is the total # parsed integers
-    //1. insert to heaps
-    if((h_high->size() == 0) || (h_low->size() == 0)){
-      
+    //1. insert to heap
+    if(h_low->size() == 0){//Case h_low is empty:
+      h_low->insert(d);
+    }else{
+      if(d <= h_low->view_max()){
+        h_low->insert(d);
+      }else{
+        h_high->insert(d);
+      }
     }
+    //rebalance
+    if(h_low->size() > h_high->size() + 1){//h_low is heavier by more than 1 element
+      //move one element of low to high
+      h_high->insert(h_low->extract_max());
+    }
+    if(h_high->size() > h_low->size()){//h_high is heavier
+      h_low->insert(h_high->extract_min());
+    }
+
+    //h_low's max is the (i/2) th element of x_1 to x_i
+    //or if i is odd, (i+1)/2 th element
+    median.push_back(h_low->view_max());
+    sum += median.back();
+    std::cout << d << " | " << median.back() << std::endl;
   }
-  std::cout << "Successfully loaded " << filename << "with " << i << " lines." << std::endl;
+  std::cout << "Successfully loaded " << filename << " with " << i << " lines." << std::endl;
+  std::cout << "Sum: " << sum << std::endl;
+  std::cout << "Sum %% 10000: " << sum % 10000 << std::endl;
   //u_graph->print_graph(9);
 }
